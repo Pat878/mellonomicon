0000d95d0c33a60d9aafaa506d67c0e68ebbbe1f
Pat Mellon <pat@shiftlabny.com>
2020-02-23 11:30:52 -0500

    Add contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
+import { navigate } from "gatsby-link"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
@@ -15,31 +16,92 @@ interface Props {
   location: Location
 }
 
-const About = ({ data }: Props) => {
+interface FormData {
+  data: {
+    name: string
+    email: string
+    message: string
+  }
+}
+
+const Contact = ({ data }: Props) => {
+  const [state, setState] = useState({})
   const siteTitle = data.site.siteMetadata.title
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const encode = (data: any) => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Contact" />
       <div>
-        <form method="post" netlify-honeypot="bot-field" data-netlify="true">
-          <input type="hidden" name="bot-field" />
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="hidden"
+            name="bot-field"
+            value="contact"
+            onChange={handleChange}
+          />
           <label>
             Name:
             <p>
-              <input type="text" name="name" id="name" />
+              <input
+                type="text"
+                name="name"
+                id="name"
+                onChange={handleChange}
+              />
             </p>
           </label>
           <label>
             Email:
             <p>
-              <input type="email" name="email" id="email" />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleChange}
+              />
             </p>
           </label>
           <label>
             Message:
             <p>
-              <textarea name="message" id="message" rows="5" />
+              <textarea
+                name="message"
+                id="message"
+                rows="5"
+                onChange={handleChange}
+              />
             </p>
           </label>
           <button type="submit" className="send-button">
@@ -52,7 +114,7 @@ const About = ({ data }: Props) => {
   )
 }
 
-export default About
+export default Contact
 
 export const pageQuery = graphql`
   query {
